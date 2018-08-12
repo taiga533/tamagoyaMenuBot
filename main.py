4589887b1d6a398db1aa704dde1b4f58c89d8425
川崎大河 <kawasakitaiga@kawasakimbp.local>
2018-08-13 03:39:47 +0900

    自動デプロイ設定

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import boto3
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
+    access to the request and response payload, including headers and
+    status code.
+
+    To scan a DynamoDB table, make a GET request with the TableName as a
+    query string parameter. To put, update, or delete an item, make a POST,
+    PUT, or DELETE request respectively, passing in the payload to the
+    DynamoDB API as a JSON body.
+    '''
+    logging.info(json.dumps(event))
+    return {
+        "statusCode": 200,
+        "body": event['body-json']['challenge'],
+    }
+    
